50e9ab28b141c09f01893255ae56e2da7d8974c9
Won <fain9301@yahoo.com>
2022-01-02 00:11:04 +0900

    feat: first commit

---
+++ src/main/java/kr/co/won/FastcampusTodoServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.won;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusTodoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusTodoServerApplication.class, args);
+    }
+
+}
