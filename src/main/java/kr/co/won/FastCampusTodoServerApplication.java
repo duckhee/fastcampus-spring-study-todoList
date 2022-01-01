c8aba080ffa95133d54717774ccb1b7a0b86e1e2
Won <fain9301@yahoo.com>
2022-01-02 00:20:42 +0900

    feat: fastcampus todo server entity set

--- src/main/java/kr/co/won/FastCampusTodoServerApplication.java
+++ src/main/java/kr/co/won/FastCampusTodoServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FastcampusTodoServerApplication {
+public class FastCampusTodoServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FastcampusTodoServerApplication.class, args);
+        SpringApplication.run(FastCampusTodoServerApplication.class, args);
     }
 
 }
